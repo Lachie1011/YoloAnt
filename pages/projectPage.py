b6daa8dcd7864f11d54d0f80cab3e1bf30d3c4e2
Lachlan Masson <lachlanmasson@gmail.com>
2023-12-26 22:00:02 +1000

    Changes to UI

--- pages/projectPage.py
+++ pages/projectPage.py
@@ -3,6 +3,11 @@
 """
 
 import sys
+
+from PyQt6 import QtWidgets
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+
 from yoloAnt_ui import Ui_MainWindow
 
 class ProjectPage():
@@ -18,8 +23,25 @@ class ProjectPage():
         """ Populates the fields for the project page from the project.yaml """
         self.ui.projectNameLbl.setText(self.app.project.name)
         self.ui.projectDescriptionLbl.setText(self.app.project.description)
-        self.ui.datasetPathTxt.setText(self.app.project.dataset)
+        self.ui.datasetPathLbl.setText(self.app.project.dataset)
 
     def loadPage(self) -> None:
         """ Loads all information and functionality """
         self.__populateFields()
+        self.__createPlot()
+
+    def __createPlot(self) -> None: 
+        """ Creates a plot """
+        # Updating plot stlying
+        self.ui.graphWidget.setBackground((65, 66, 64))
+        self.ui.graphWidget.setStyleSheet("border : 1px solid; border-color: rgb(65, 66, 64);")
+    
+        # TODO: placeholder plot atm
+        xData = [1,2,3,4,5,6,7,8,9,10]
+        yData = [30,32,34,32,33,31,29,32,35,45]
+
+        # plot data: x, y values
+        pen = pg.mkPen(color=(255,255,200), width=2)
+        self.ui.graphWidget.plotItem.getAxis('left').setPen(pen)
+        self.ui.graphWidget.plotItem.getAxis('bottom').setPen(pen)
+        self.ui.graphWidget.plot(xData, yData, pen=pen)
