25dff8895ec2e7b193a544ad246bef9a69c69cfa
Lachan Masson <lachlanmasson@gmail.com>
2024-01-07 20:27:57 +1000

    About to be refactored lol

--- customWidgets/annotationCanvasWidget.py
+++ customWidgets/annotationCanvasWidget.py
@@ -2,21 +2,34 @@
     annotationCanvasWidget.py
 """
 
-from PyQt6.QtGui import QPixmap, QPixmap, QPainter, QPen, QColor
+from PyQt6.QtCore import Qt
+from PyQt6.QtCore import QPoint, QRect
+from PyQt6.QtGui import QPixmap, QPixmap, QPainter, QPen, QColor, QCursor
 from PyQt6.QtWidgets import QWidget, QLabel, QHBoxLayout
 
 
 class AnnotationCanvasWidget(QWidget):
     """ A custom widget for the annotation canvas used for drawing bounding boxes """
-    def __init__(self, parent=None):
+    def __init__(self, parent):
         super(AnnotationCanvasWidget, self).__init__(parent)
 
         # Members
-        self.currentImagePath = None  # TODO: update as a list of images? 
-        self.currentImagePath = "/home/lachie/extractedImages/1029.jpg"
+        self.rectBegin = QPoint()
+        self.rectEnd = QPoint()
+        self.currentImagePath = "/home/lachie/extractedImages/1029.jpg"  #TODO: this will need to be on each new image 
+
+        ## TO BE CHANGED
+        self.pixmap_image = QPixmap(self.currentImagePath)
+
+        # set rectangle color and thickness TODO: this will be based off of a class colour
+        self.penRectangle = QPen(QColor(0, 0, 0))
+        self.penRectangle.setWidth(3)
+        ## END TO BE CHANGED
 
         # Constructing the widget
         self.canvasLabel = QLabel()
+        self.canvasLabel.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        self.canvasLabel.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
         # Construcing the layout
         self.canvasLayout = QHBoxLayout()
@@ -24,23 +37,34 @@ class AnnotationCanvasWidget(QWidget):
 
         self.setLayout(self.canvasLayout)
 
-        self.drawRectangle()
-
-    def drawRectangle(self):
-        """ A test class to draw rectangles on a label """
-        # set rectangle color and thickness
-        self.penRectangle = QPen(QColor(0, 0, 0))
-        self.penRectangle.setWidth(3)
-     
-        # convert image file into pixmap
+    def paintEvent(self, event):
+        """ A paint event to draw rectangles """
+        # Update image pixmap to reset canvas
         self.pixmap_image = QPixmap(self.currentImagePath)
+        
+        # Create painter instance with pixmap
+        painterInstance = QPainter(self.pixmap_image)
 
-        # create painter instance with pixmap
-        self.painterInstance = QPainter(self.pixmap_image)
+        # Draw rectangle on painter
+        painterInstance.setPen(self.penRectangle)
+        painterInstance.drawRect(QRect(self.rectBegin, self.rectEnd))
 
-        # draw rectangle on painter
-        self.painterInstance.setPen(self.penRectangle)
-        self.painterInstance.drawRect(0, 0, 500, 500)
-
-        # set pixmap onto the label widget
+        # Set pixmap onto the label widget
         self.canvasLabel.setPixmap(self.pixmap_image)
+
+    def mousePressEvent(self, event):
+        """ Event to capture mouse press and update rect coords """
+        self.rectBegin = event.pos()
+        self.rectEnd = event.pos()
+        self.update()
+
+    def mouseMoveEvent(self, event):
+        """ Event to capture mouse move and update rect coords """
+        self.rectEnd = event.pos()
+        self.update()
+
+    def mouseReleaseEvent(self, event):
+        """ Event to capture mouse release and update rect coords """
+        self.rectBegin = event.pos()
+        self.rectEnd = event.pos()
+        self.update()
