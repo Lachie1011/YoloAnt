388438efa372f2e7039a9b51d863b65044896197
Lachan Masson <lachlanmasson@gmail.com>
2024-01-08 20:40:35 +1000

    Scene takes up widget

--- customWidgets/annotationCanvasWidget.py
+++ customWidgets/annotationCanvasWidget.py
@@ -2,6 +2,8 @@
     annotationCanvasWidget.py
 """
 
+import time
+
 from PyQt6.QtCore import Qt
 from PyQt6.QtCore import QPoint, QRect
 from PyQt6.QtGui import QPixmap, QPixmap, QPainter, QPen, QColor, QCursor
@@ -14,35 +16,21 @@ class AnnotationCanvasWidget(QGraphicsView):
         super(AnnotationCanvasWidget, self).__init__(parent)
 
         # Members
-        self.rectBegin = QPoint()
-        self.rectEnd = QPoint()
+        self.rectBegin = None
+        self.rectEnd = None
         self.currentImagePath = "/home/lachie/extractedImages/1029.jpg"  #TODO: this will need to be on each new image 
 
         # Setting alignment
-        self.setAlignm
+        self.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
 
-        # Creating the graphics view and scene
+        # Creating the graphics scene
         self.scene = QGraphicsScene()
+
+        # Scene is bound to graphics view geometry
+        self.scene.setSceneRect(self.x(), self.y(), self.width(), self.height())
         self.scene.setBackgroundBrush(QColor(255,255,255))
 
         self.setScene(self.scene)
-
-    # def paintEvent(self, event):
-    #     """ A paint event to draw rectangles """
-    #     # Update image pixmap to reset canvas
-    #     self.pixmap_image = QPixmap(self.currentImagePath)
-        
-    #     # Create painter instance with pixmap
-    #     painterInstance = QPainter(self.pixmap_image)
-
-    #     # Draw rectangle on painter
-    #     painterInstance.setPen(self.penRectangle)
-    #     painterInstance.drawRect(QRect(self.rectBegin, self.rectEnd))
-
-    #     # Set pixmap onto the label widget
-    #     self.canvasLabel.setPixmap(self.pixmap_image)
-
-    # self.scene.addRect(-0,-0, 200,200)
 
     def mousePressEvent(self, event):
         """ Event to capture mouse press and update rect coords """
@@ -53,14 +41,6 @@ class AnnotationCanvasWidget(QGraphicsView):
         """ Event to capture mouse move and update rect coords """
         self.rectEnd = self.mapToScene(event.pos())
         self.scene.clear()
-        print(self.rectBegin.x())
-        print(self.rectBegin.y())
-        print(self.rectEnd.x())
-        print(self.rectEnd.y())
-        print("width: " + str(abs(self.rectEnd.x() - self.rectBegin.x())))
-        print("height: " + str(abs(self.rectEnd.y() - self.rectBegin.y())))
-
-
         self.scene.addRect(self.rectBegin.x(), self.rectBegin.y(), abs(self.rectEnd.x() - self.rectBegin.x()), abs(self.rectEnd.y() - self.rectBegin.y()))
 
     def mouseReleaseEvent(self, event):
